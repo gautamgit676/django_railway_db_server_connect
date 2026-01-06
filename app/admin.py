568909e829213efedd8a96b6832d07aab0ec09c8
gautamgit676 <gautamgit244@gmail.com>
2026-01-06 18:07:45 +0530

    add db

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from app.models import Usercustome
+
+
+class CustomUserAdmin(UserAdmin):
+    model = Usercustome
+    list_display = ('id','username', 'email', 'phone_number')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone_number', 'user_role')}),
+    )
+    
+admin.site.register(Usercustome, CustomUserAdmin)
+    
+    
+    
--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class Usercustome(AbstractUser):
     ROLE_CHOICES = (
         ('shopkeeper', 'Shopkeeper'),
         ('customer', 'Customer'),
