895b5fe37c65f69112be9974dc817ddaa73bd769
gautamgit676 <gautamgit244@gmail.com>
2026-01-06 19:23:54 +0530

    add user data daa

--- app/views.py
+++ app/views.py
@@ -8,3 +8,19 @@ def home(request):
     return render(request, 'home.html', {'user': user})
 
 
+def creaetusers(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        phone_number = request.POST.get('phone_number')
+        role = request.POST.get('role')
+        password = request.POST.get('password')
+
+        Usercustome.objects.create(
+            username=username,
+            email=email,
+            phone_number=phone_number,
+            role=role,
+            password=password
+        )
+    return render(request, 'create_user.html')
