ed0677fabf19dbaa2d00cde2fc23404b37ecbd6f
gautamgit676 <gautamgit244@gmail.com>
2026-01-07 14:41:54 +0530

    add new apis

--- app/views.py
+++ app/views.py
@@ -71,6 +71,12 @@ class userlogout(APIView):
         
         
 class UserProfile(APIView):
+    
+    def get(self, request):
+        profiles = UserProfile.objects.all()
+        serializer = UserProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+    
     def post(self, request):
         serializer = UserProfileSerializer(data=request.data)
         if serializer.is_valid():
