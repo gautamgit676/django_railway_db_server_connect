cab34cf6277495cc8a021864332c5387858b11d3
gautamgit676 <gautamgit244@gmail.com>
2026-01-07 14:48:52 +0530

    add new apis

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app.models import Usercustome
+from app.models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login,logout
